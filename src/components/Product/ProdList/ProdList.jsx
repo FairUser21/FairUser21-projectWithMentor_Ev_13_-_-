b789b775379df5396dea39675d58ca3fc39b6313
FairUser21 <renat.alimbekov2002@gmail.com>
2022-12-15 19:03:21 +0600

    .

--- src/components/Product/ProdList/ProdList.jsx
+++ src/components/Product/ProdList/ProdList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useContext, useEffect } from "react";
+import { productContext } from "../../../contexts/ProductContextProvider";
+import OneProd from "../OneProd/OneProd";
 
 const ProdList = () => {
-  return <div></div>;
+  const { products, getProducts } = useContext(productContext);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <Box>
+      {products ? products.map((item) => <OneProd item={item} />) : null}
+    </Box>
+  );
 };
 
 export default ProdList;
